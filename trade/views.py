c208c8f39a3aeb1eb87974162d6a8ef58c1a9cef
ovladychenko <ovladychenko@gmail.com>
2023-02-14 00:55:45 +0200

    first commit

--- trade/views.py
+++ trade/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Страница торговля")
+
+def catigories(request):
+    return HttpResponse("Страница торговля")
