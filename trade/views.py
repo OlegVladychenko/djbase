69c5f17e675d27c01d6c065a7c95978499a922ed
ovladychenko <ovladychenko@gmail.com>
2023-02-27 23:39:22 +0200

    commit

--- trade/views.py
+++ trade/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Страница торговля")
+    return HttpResponse("Страница главная")
 
 def catigories(request):
-    return HttpResponse("Страница торговля")
+    return HttpResponse("<h1>Страница торговля</h1>")
