f8e11b7519208cc1894e3ec456810ff3f230326b
ovladychenko <ovladychenko@gmail.com>
2023-03-13 23:15:19 +0200

    commit

--- trade/views.py
+++ trade/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
+from .models import *
+
 menu = ["Контрагенты","Номенклатура","Торговые"]
 
 def index(request):
     return render(request, 'trade/index.html',{'menu':menu})
 
 def сlients(request):
-    return render(request, 'trade/clients.html')
+    return render(request, 'trade/clients.html',{'menu':menu})
 
 def pageNotFound(request, exeption):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
